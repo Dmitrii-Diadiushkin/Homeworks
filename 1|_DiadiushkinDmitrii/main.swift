ff780d5f702a1d0bd31a5663f31587228df6101c
Dmitrii Diadiushkin <stephandvd@yandex.ru>
2020-03-18 22:47:30 +0500

    Initial Commit

---
+++ 1|_DiadiushkinDmitrii/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  1|_DiadiushkinDmitrii
+//
+//  Created by Dmitriy Dyadyushkin on 18.03.2020.
+//  Copyright © 2020 Dmitriy Dyadyushkin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
